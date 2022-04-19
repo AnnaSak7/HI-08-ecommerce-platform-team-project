072eac906090a9aba946edfc791ed2b5695835a7
Anna Sakurai <zenasak@gmail.com>
2022-04-20 01:58:20 +0200

    add Form page to add new products

---
+++ memory_ecommerce/pages/new-product/index.js
@@ -0,0 +1,41 @@
+
+// our-domain.com/new-meetup
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+import NewProductForm from '../../components/products/NewProductForm';
+
+
+const NewProductPage = () => {
+  const router = useRouter();
+
+  async function addProductHandler(enteredProductData) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredProductData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push('/');
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Add a New Product</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewProductForm onAddProduct={addProductHandler} />
+    </>
+  );
+};
+
+export default NewProductPage;
