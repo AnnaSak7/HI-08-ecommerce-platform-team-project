4cc06e92d6b4f89a0f872b09966dfa1e3a46d5d9
Anna Sakurai <zenasak@gmail.com>
2022-04-20 01:31:29 +0200

    create Product.module.css. fix text box styling.

--- memory_ecommerce/components/products/ProductCardOdd.js
+++ memory_ecommerce/components/products/ProductCardOdd.js
@@ -2,15 +2,17 @@ import Image from 'next/image'
 import React from 'react'
 import img01 from '../../public/product01.png'
 
+import styles from '../../styles/Product.module.css'
+
 const ProductCardOdd = (props) => {
   console.log("props", props.image)
   return (
-    <div className='odd-container'>
-        <div className='image-container'>
-        <img className="img-card" src={props.image} alt={props.name} width="500px" height="300px" />
+    <div className={styles.oddContainer}>
+        <div className={styles.imageContainer}>
+        <img src={props.image} alt={props.name} width="500px" height="300px" />
         </div>
-        <div className="textBox" style={{backgroundColor: props.color}}>
-          <h2>{props.name}</h2>
+        <div className={styles.textBox} style={{backgroundColor: props.color}}>
+          <h2 className={styles.productName}>{props.name}</h2>
             <p>{props.description}</p>
         </div>
     </div>
--- memory_ecommerce/pages/index.js
+++ memory_ecommerce/pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import ProductList from "../components/products/ProductList";
 import Navbar from "../components/ui/Navbar";
@@ -5,11 +6,17 @@ import Hero from '../public/HeroImage.png'
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Memory | Home</title>
+        <meta name='description' content="Memory store" />
+      </Head>
     <div>
       <div id="hero">
-        <Image src={Hero} alt="Hero" />
+        <Image id="hero-image" src={Hero} alt="Hero" />
       </div>
       <ProductList />
     </div>
+    </>
   )
 }
