ba4077fb880147d67d1691847c021c5640a9cc91
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2022-04-19 12:02:54 +0200

    fixed .env + .gitignore

--- memory_ecommerce/components/ui/Navbar.js
+++ memory_ecommerce/components/ui/Navbar.js
@@ -1,25 +1,8 @@
-
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <nav>
-      {/* <div className="logo">
-        <Image src="/logo.png" width={128} height={77} />
-      </div> */}
-
-      <Link href="/">
-        <a>Anime List</a>
-      </Link>
-      <Link href="/anime">
-        <a>About</a>
-      </Link>
-      <Link href="/new-anime">
-        <a>Add New</a>
-      </Link>
-    </nav>
-  );
+  return <nav></nav>;
 };
 
 export default Navbar;
