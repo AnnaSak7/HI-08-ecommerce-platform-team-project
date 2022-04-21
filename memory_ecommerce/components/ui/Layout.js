e06cc0c47db125952a121b36df2391d7e2f2f3d3
Tintin Hamrin <tintinhamrin@gmail.com>
2022-04-21 09:45:06 +0200

    added a separate productsNavBar and  added the component to the layout

--- memory_ecommerce/components/ui/Layout.js
+++ memory_ecommerce/components/ui/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
+import ProductsBar from './ProductsBar'
 
 const Layout = ({children}) => {
   return (
     <div>
         <Navbar />
+        <ProductsBar />
         {children}
         <Footer />
     </div>
