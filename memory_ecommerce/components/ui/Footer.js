c9c6db3dae0e71bb5f826aba90ec1b2f6195d16f
LinaSimon <90833662+LinaSimon@users.noreply.github.com>
2022-04-21 11:33:09 +0200

    final footer

--- memory_ecommerce/components/ui/Footer.js
+++ memory_ecommerce/components/ui/Footer.js
@@ -8,8 +8,9 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import TextField from '@mui/material/TextField';
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import ChatIcon from "@mui/icons-material/Chat";
 
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -19,77 +20,99 @@ import Button from '@mui/material/Button';
 
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#2C3F40",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: "#B5E0DF",
+}));
   
-
 
 export default function AutoGrid() {
   return (
-
-    <Box sx={{ flexGrow: 1 ,padding: '2rem', bgcolor:'#2C3F40'}}>
-      <Typography style={{textAlign:'center', padding:'10px', color: '#fff', fontSize:'1.5em'}}>
-          CONTACT US
+    <Box sx={{ flexGrow: 1, padding: "2rem", bgcolor: "#2C3F40" }}>
+      <Typography
+        style={{
+          textAlign: "center",
+          padding: "10px",
+          color: "#B5E0DF",
+          fontSize: "1.5em",
+        }}
+      >
+        CONTACT US
       </Typography>
 
       <Grid container spacing={3}>
         <Grid item xs>
-          <Item>FOLLOW US <br />
-     
-
-          <BottomNavigationAction sx={{color:'#000'}} icon={<FacebookIcon />} /> 
-                                
-         <BottomNavigationAction  sx={{color:'#000'}}icon={<InstagramIcon />} />
-                       
-         <BottomNavigationAction sx={{color:'#000'}} icon={<SubscriptionsIcon />} />
-
-        
-       
+          <Item elevation={0}>
+            FOLLOW US <br />
+            <BottomNavigationAction
+              sx={{ color: "#B5E0DF" }}
+              icon={<FacebookIcon />}
+            />
+            <BottomNavigationAction
+              sx={{ color: "#B5E0DF" }}
+              icon={<InstagramIcon />}
+            />
+            <BottomNavigationAction
+              sx={{ color: "#B5E0DF" }}
+              icon={<SubscriptionsIcon />}
+            />
           </Item>
         </Grid>
 
-
-        <Grid item xs={6} >
-          <Item style={{JustifyContent:'center'}}>
-              
-          NEWSLETTER <br/>
-          Sign up for our newsletter and get emails you love filled
-          with great inspirations and tips of the latest memories releases and
-          the most recommended. 
-
-          <Box 
-           component="form"
-             sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-             }}
-                 noValidate
-            autoComplete="off"
+        <Grid item xs={6}>
+          <Item elevation={0} style={{ justifyItems: "center" }}>
+            <Typography>
+              NEWSLETTER <br />
+              Sign up for our newsletter and get emails you love filled with
+              great inspirations and tips of the latest memories releases and
+              the most recommended.
+            </Typography>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                style={{ backgroundColor: "#fff", borderRadius: "5px" }}
+              />
+              <br />
+              <Button
+                variant="outlined"
+                style={{ backgroundColor: "#5FA0A4", color: "#fff" }}
               >
-                 <TextField id="outlined-basic" label="Email" variant="outlined" />
-                 
-                 <Stack spacing={2} direction="row">
-            
-                 <Button  variant="outlined">SUBMIT</Button>
-                </Stack>
-              </Box>
-
-             
-
-
+                SUBMIT
+              </Button>
+              <Stack spacing={2} direction="row"></Stack>
+            </Box>
           </Item>
+          <Typography style={{ textAlign: "center", color: "#B5E0DF" }}>
+            2022 ® Memory X <br /> All memories are owned by Memory X
+          </Typography>
         </Grid>
-
 
         <Grid item xs>
-          <Item>WRITE US</Item>
+          <Item elevation={0}>
+            <Typography>WRITE US</Typography>
+            <br />
+            <BottomNavigationAction
+              sx={{ color: "#B5E0DF" }}
+              icon={<MailOutlineIcon />}
+            />
+            <BottomNavigationAction
+              sx={{ color: "#B5E0DF" }}
+              icon={<ChatIcon />}
+            />
+          </Item>
         </Grid>
       </Grid>
-
     </Box>
-   
   );
 }
