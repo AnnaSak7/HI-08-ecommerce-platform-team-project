1b315e48c086da683680255072c0c527f0f4e022
Anna Sakurai <zenasak@gmail.com>
2022-04-21 21:27:59 +0200

    fix .env file

--- memory_ecommerce/pages/index.js
+++ memory_ecommerce/pages/index.js
@@ -3,42 +3,39 @@ import Head from "next/head";
 
 import ProductList from "../components/products/ProductList";
 import Navbar from "../components/ui/Navbar";
-import Hero from '../components/ui/Hero'
-
+import Hero from "../components/ui/Hero";
 
 export default function Home(props) {
-  console.log(props.memories)
+  console.log(props.memories);
   return (
     <>
       <Head>
         <title>Memory | Home</title>
-        <meta name='description' content="Memory store" />
+        <meta name="description" content="Memory store" />
       </Head>
-    <Hero />
-      <ProductList memories={props.memories}/>
-    
+      <Hero />
+      <ProductList memories={props.memories} />
     </>
-  )
+  );
 }
 
-
 export const getStaticProps = async () => {
-  const client = await MongoClient.connect(process.env.MONGO_URI)
-  const db = client.db()
-  const memoryCollection = db.collection('memories')
-  const memories = await memoryCollection.find().toArray()
+  const client = await MongoClient.connect(process.env.MONGO_URI);
+  const db = client.db();
+  const memoryCollection = db.collection("memories");
+  const memories = await memoryCollection.find().toArray();
 
-  client.close()
+  client.close();
 
-  return{
+  return {
     props: {
-      memories: memories.map(memory => ({
+      memories: memories.map((memory) => ({
         id: memory._id.toString(),
         name: memory.name,
         image: memory.image,
-        description: memory.description
-      }))
+        description: memory.description,
+      })),
     },
     revalidate: 10,
-  }
-}
+  };
+};
