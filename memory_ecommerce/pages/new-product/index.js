7207eb40da8e7014a4f5ac7b77a3bcc71f546dcf
Anna Sakurai <zenasak@gmail.com>
2022-04-25 12:24:23 +0200

    add form to add products to MongoDB and added products data

---
+++ memory_ecommerce/pages/new-product/index.js
@@ -0,0 +1,39 @@
+// our-domain.com/new-product
+import { useRouter } from "next/router";
+import Head from "next/head";
+import NewProductForm from "../../components/forms/NewProductForm";
+
+const NewProductPage = () => {
+  const router = useRouter();
+
+  async function addProductHandler(enteredProductData) {
+    const response = await fetch("/api/new-product", {
+      method: "POST",
+      body: JSON.stringify(enteredProductData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Add a New Product</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewProductForm onAddProduct={addProductHandler} />
+    </>
+  );
+};
+
+export default NewProductPage;
