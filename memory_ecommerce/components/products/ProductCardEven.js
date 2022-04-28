8414e0790ec39752afe3b4474c16815b3090f211
Anna Sakurai <zenasak@gmail.com>
2022-04-28 10:37:19 +0200

    try

--- memory_ecommerce/components/products/ProductCardEven.js
+++ memory_ecommerce/components/products/ProductCardEven.js
@@ -1,22 +1,32 @@
-import Image from 'next/image'
-import React from 'react'
-import img01 from '../../public/product01.png'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import img01 from "../../public/product01.png";
 
-import styles from '../../styles/Product.module.css'
+import styles from "../../styles/Product.module.css";
 
 const ProductCardEven = (props) => {
-  console.log('name is ', props.name)
+  console.log("name is ", props.name);
   return (
     <div className={styles.evenContainer}>
-        <div className={styles.imageContainer}>
-        <img src={props.image} alt={props.name} width="500px" height="300px" />
-        </div>
-        <div className={styles.textBox} style={{backgroundColor: props.color}}>
+      <div className={styles.imageContainer}>
+        <Link href={`/${props.id}`}>
+          <a>
+            <img
+              src={props.image}
+              alt={props.name}
+              width="500px"
+              height="300px"
+            />
+          </a>
+        </Link>
+      </div>
+      <div className={styles.textBox} style={{ backgroundColor: props.color }}>
         <h2 className={styles.productName}>{props.name}</h2>
-            <p>{props.description}</p>
-        </div>
+        <p>{props.description}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCardEven
+export default ProductCardEven;
